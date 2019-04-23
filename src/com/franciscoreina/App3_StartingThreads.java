4b7208bea1139a8d5fce8a540f4f0fb2c4f9babc
franciscoreinaponce <franciscoreinaponce@gmail.com>
2019-04-23 22:06:35 +0100

    Ways to create a thread

---
+++ src/com/franciscoreina/App3_StartingThreads.java
@@ -0,0 +1,28 @@
+package com.franciscoreina;
+
+// Implementation of threads, implementing Runnable with anonymous class.
+
+public class App3_StartingThreads {
+
+	public static void main(String[] args) {
+		Thread t1 = new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				for (int i = 0; i < 10; i++) {
+					System.out.println("Hello " + i);
+
+					try {
+						Thread.sleep(100);
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+			}
+		});
+
+		t1.start();
+	}
+
+}
